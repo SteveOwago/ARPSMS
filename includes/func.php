1d94c093b4592aaaa611f13d14a9c93b52bdde37
SteveOwago <33925746+SteveOwago@users.noreply.github.com>
2019-12-13 11:14:32 +0300

    Revert "Merge branch 'branch1'" This reverts commit 61cd930c32187003074801a042b363d00bc089bf, reversing changes made to 3421b4a6cc65607a5e1d08209fc607f0473b5674.

--- includes/func.php
+++ includes/func.php
@@ -83,7 +83,7 @@ if (isset($_POST['submit'])) {
 			require '../PHPMailer/class.smtp.php';
 			$mail = new PHPMailer;
 			$mail->setFrom('admin@example.com');
-			$mail->addAddress($email);
+			$mail->addphone2($email);
 			$mail->Subject = 'Verification Email';
 			$mail->Body = "Please click on the link below to verfy your account:<br><br>
 				<a href='127.0.0.1/ARPSMS/includes/emailverification.php?email=$email&token=$token'>Click Here</a>
@@ -94,10 +94,10 @@ if (isset($_POST['submit'])) {
 			$mail->SMTPAuth = true;
 			$mail->Port = 465;
 
-			//Set your existing gmail address as user name
+			//Set your existing gmail phone2 as user name
 			$mail->Username = 'martinallen722@gmail.com';
 
-			//Set the password of your gmail address here
+			//Set the password of your gmail phone2 here
 			$mail->Password = 'allenmartin10224';
 			if(!$mail->send()) {
 				echo 'Email is not sent.';
@@ -239,8 +239,8 @@ $email = mysqli_real_escape_string($conn, $email);
 $phone = trim($_POST['phone']);
 $phone = mysqli_real_escape_string($conn, $phone);
 
-$address = trim($_POST['address']);
-$address = mysqli_real_escape_string($conn, $address);
+$phone2 = trim($_POST['phone2']);
+$phone2 = mysqli_real_escape_string($conn, $phone2);
 
 $scheme_id = trim($_POST['scheme_id']);
 $scheme_id = mysqli_real_escape_string($conn, $scheme_id);
@@ -255,7 +255,7 @@ $user_id = trim($_POST['user_id']);
 $user_id = mysqli_real_escape_string($conn, $user_id);
 
 
-if (empty($address) || empty($scheme_id) || empty($fno) || empty($fsize) || empty($user_id) )
+if (empty($phone2) || empty($scheme_id) || empty($fno) || empty($fsize) || empty($user_id) )
 
 	 	{
 		 header("Location:../register_farm.php?error=EmptyFields");
@@ -271,7 +271,7 @@ if (empty($address) || empty($scheme_id) || empty($fno) || empty($fsize) || empt
 															 echo "<script type='text/javascript' class='alert alert-success'>alert('Unsuccessful! Farm already exist,please try again!')</script>";
 																		echo '<meta http-equiv="refresh" content="0; url=../farmer_page.php">';
 														}else	{
-																		$sql = "INSERT INTO farms (phone,address,scheme_id,fnumber,farm_size,user_id) VALUES('$phone','$address','$scheme_id','$fno','$fsize','$user_id');";
+																		$sql = "INSERT INTO farms (phone,phone2,scheme_id,fnumber,farm_size,user_id) VALUES('$phone','$phone2','$scheme_id','$fno','$fsize','$user_id');";
 																		$result1 = mysqli_query($conn,$sql);
 
 																		if ($result1 == TRUE) {
@@ -383,7 +383,7 @@ if ($email == $emailData) {
 			require '/usr/share/php/libphp-phpmailer/class.smtp.php';
 			$mail = new PHPMailer;
 			$mail->setFrom('admin@example.com');
-			$mail->addAddress($email);
+			$mail->addphone2($email);
 			$mail->Subject = 'Password Reset';
 			$mail->Body = $output;
 			$mail->IsSMTP('true');
@@ -392,10 +392,10 @@ if ($email == $emailData) {
 			$mail->SMTPAuth = true;
 			$mail->Port = 465;
 
-			//Set your existing gmail address as user name
+			//Set your existing gmail phone2 as user name
 			$mail->Username = 'martinallen722@gmail.com';
 
-			//Set the password of your gmail address here
+			//Set the password of your gmail phone2 here
 			$mail->Password = 'allenmartin10224';
 			if(!$mail->send()) {
 				 header("Location:../reset-password.php?error=notsent");
@@ -434,7 +434,7 @@ if ($email == $emailData) {
 			require '/usr/share/php/libphp-phpmailer/class.smtp.php';
 			$mail = new PHPMailer;
 			$mail->setFrom('admin@example.com');
-			$mail->addAddress($email);
+			$mail->addphone2($email);
 			$mail->Subject = 'Password Recovery';
 			$mail->Body = $output;
 			$mail->IsSMTP('true');
@@ -443,10 +443,10 @@ if ($email == $emailData) {
 			$mail->SMTPAuth = true;
 			$mail->Port = 465;
 
-			//Set your existing gmail address as user name
+			//Set your existing gmail phone2 as user name
 			$mail->Username = 'martinallen722@gmail.com';
 
-			//Set the password of your gmail address here
+			//Set the password of your gmail phone2 here
 			$mail->Password = 'allenmartin10224';
 			if(!$mail->send()) {
 				header("Location:../reset-password.php?error=notsent");
@@ -464,64 +464,8 @@ exit();
 }
 
 ?>
-<?php
-
-//Add items
-
-//Register Farm
 
 
-
-if (isset($_POST['register_farm'])) {
-
-
-$fname = trim($_POST['fname']);
-$lname = trim($_POST['lname']);
-$uname = trim($_POST['uname']);
-$email = trim($_POST['email']);
-$phone = trim($_POST['phone']);
-$address = trim($_POST['address']);
-$scheme_id = trim($_POST['scheme_id']);
-$fno = trim($_POST['fno']);
-$fsize= trim($_POST['fsize']);
-$user_id = trim($_POST['user_id']);
-
-if (empty($phone) || empty($address) || empty($scheme_id) || empty($fno) || empty($fsize) || empty($user_id))
-
-	 {
-		 header("Location:../register_farm.php?error=EmptyFields");
-		 exit();
-	}
-	else
-	{
-		$sql = "SELECT * FROM register_db JOIN farms ON register_db.user_id = farms.user_id WHERE email = '$email' && fnumber='$fno'";
-		$result = mysqli_query($conn,$sql);
-		$check = mysqli_num_rows($result);
-
-		if ($check > 0) {
-			 echo "<script type='text/javascript' class='alert alert-success'>alert('Unsuccessful! Farm already exist,please try again!')</script>";
-						echo '<meta http-equiv="refresh" content="0; url=../index.php">';
-		}
-
-	else
-	{
-		$sql = "INSERT INTO farms (phone,address,scheme_id,fnumber,farm_size,user_id) VALUES('$phone','$address','$scheme_id','$fno','$fsize','$user_id');";
-		$result = mysqli_query($conn,$sql);
-
-		if ($result == TRUE) {
-
-						echo "<script type='text/javascript' class='alert alert-success'>alert('Submitted successfully! Thanks for registering with us!')</script>";
-						echo '<meta http-equiv="refresh" content="0; url=../farmer_page.php">';
-		}
-		else
-		{
-			 echo "string" .$conn->error;
-		}
-	}
-}
-}
-
-?>
 <?php
 
 //Add items
@@ -575,7 +519,7 @@ if ($email == $emailData) {
 			require '/usr/share/php/libphp-phpmailer/class.smtp.php';
 			$mail = new PHPMailer;
 			$mail->setFrom('admin@example.com');
-			$mail->addAddress($email);
+			$mail->addphone2($email);
 			$mail->Subject = 'Password Reset';
 			$mail->Body = $output;
 			$mail->IsSMTP('true');
@@ -584,10 +528,10 @@ if ($email == $emailData) {
 			$mail->SMTPAuth = true;
 			$mail->Port = 465;
 
-			//Set your existing gmail address as user name
+			//Set your existing gmail phone2 as user name
 			$mail->Username = 'martinallen722@gmail.com';
 
-			//Set the password of your gmail address here
+			//Set the password of your gmail phone2 here
 			$mail->Password = 'allenmartin10224';
 			if(!$mail->send()) {
 				echo 'Email is not sent.';
@@ -626,7 +570,7 @@ if ($email == $emailData) {
 			require '/usr/share/php/libphp-phpmailer/class.smtp.php';
 			$mail = new PHPMailer;
 			$mail->setFrom('admin@example.com');
-			$mail->addAddress($email);
+			$mail->addphone2($email);
 			$mail->Subject = 'Password Recovery';
 			$mail->Body = $output;
 			$mail->IsSMTP('true');
@@ -635,10 +579,130 @@ if ($email == $emailData) {
 			$mail->SMTPAuth = true;
 			$mail->Port = 465;
 
-			//Set your existing gmail address as user name
+			//Set your existing gmail phone2 as user name
 			$mail->Username = 'martinallen722@gmail.com';
 
-			//Set the password of your gmail address here
+			//Set the password of your gmail phone2 here
+			$mail->Password = 'allenmartin10224';
+			if(!$mail->send()) {
+				echo 'Email is not sent.';
+				echo 'Email error: ' . $mail->ErrorInfo;
+			} else {
+				 header("Location:../reset-password.php?error=sent");
+			exit();
+			}
+}
+else
+{
+header("Location:../reset-password.php?fatalerror");
+exit();
+}
+}
+
+
+
+
+
+
+if ($result)
+ {
+	header("Location:../add_item.php?error=Successful");
+	exit();
+}
+else
+{
+	echo "string".$conn->error;
+}
+}
+}
+
+
+//Change Password
+if (isset($_POST['submit_reset'])) {
+
+$email = mysqli_real_escape_string($conn,$_POST['email']);
+
+$sql = "SELECT * FROM register_db WHERE email='$email'";
+$result = mysqli_query($conn,$sql);
+$count = mysqli_num_rows($result);
+$data = mysqli_fetch_array($result);
+$emailData =$data['email'];
+$user_id = $data['user_id'];
+
+$url = 'http://'.$_SERVER['SERVER_NAME'].'/ARPSMS/password_reset.php?user_id='.$user_id.'&email='.$emailData;
+$output = 'Hi. Please click this link to change your password.<br>'.$url;
+
+if ($email == $emailData) {
+
+
+							require '/usr/share/php/libphp-phpmailer/class.phpmailer.php';
+			require '/usr/share/php/libphp-phpmailer/class.smtp.php';
+			$mail = new PHPMailer;
+			$mail->setFrom('admin@example.com');
+			$mail->addphone2($email);
+			$mail->Subject = 'Password Reset';
+			$mail->Body = $output;
+			$mail->IsSMTP('true');
+			$mail->SMTPSecure = 'ssl';
+			$mail->Host = 'ssl://smtp.gmail.com';
+			$mail->SMTPAuth = true;
+			$mail->Port = 465;
+
+			//Set your existing gmail phone2 as user name
+			$mail->Username = 'martinallen722@gmail.com';
+
+			//Set the password of your gmail phone2 here
+			$mail->Password = 'allenmartin10224';
+			if(!$mail->send()) {
+				echo 'Email is not sent.';
+				echo 'Email error: ' . $mail->ErrorInfo;
+			} else {
+				 header("Location:../reset-password.php?error=sent");
+			exit();
+			}
+}
+else
+{
+header("Location:../reset-password.php?fatalerror");
+exit();
+}
+}
+
+//Forgot Password -Send mail
+if (isset($_POST['submit_forgot'])) {
+
+$email = mysqli_real_escape_string($conn,$_POST['email']);
+
+$sql = "SELECT * FROM register_db WHERE email='$email'";
+$result = mysqli_query($conn,$sql);
+$count = mysqli_num_rows($result);
+$data = mysqli_fetch_array($result);
+$emailData =$data['email'];
+$user_id = $data['user_id'];
+
+$url = 'http://'.$_SERVER['SERVER_NAME'].'/ARPSMS/forgot_password.php?user_id='.$user_id.'&email='.$emailData;
+$output = 'Hi. Please click this link to reset your password.<br>'.$url;
+
+if ($email == $emailData) {
+
+
+							require '/usr/share/php/libphp-phpmailer/class.phpmailer.php';
+			require '/usr/share/php/libphp-phpmailer/class.smtp.php';
+			$mail = new PHPMailer;
+			$mail->setFrom('admin@example.com');
+			$mail->addphone2($email);
+			$mail->Subject = 'Password Recovery';
+			$mail->Body = $output;
+			$mail->IsSMTP('true');
+			$mail->SMTPSecure = 'ssl';
+			$mail->Host = 'ssl://smtp.gmail.com';
+			$mail->SMTPAuth = true;
+			$mail->Port = 465;
+
+			//Set your existing gmail phone2 as user name
+			$mail->Username = 'martinallen722@gmail.com';
+
+			//Set the password of your gmail phone2 here
 			$mail->Password = 'allenmartin10224';
 			if(!$mail->send()) {
 				echo 'Email is not sent.';
